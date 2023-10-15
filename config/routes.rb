163f6ef76381be308ef1534fc128aa2f672d2716
kolesa <kolesaak@gmail.com>
2023-10-15 15:43:03 -0700

    Added Weather controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "weathers#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'weathers/show', to: 'weathers#show', as: 'weather_show'
+  resources :weathers, only: :index
 end
