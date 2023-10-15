e81f6128e5553403e6a6c49d515d9e11b2c9d20a
kolesa <kolesaak@gmail.com>
2023-10-15 16:02:11 -0700

    Added actions index and show to WeathersController

--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,2 +1,15 @@
 class WeathersController < ApplicationController
+
+  def index; end
+
+  def show
+    zip_code = params[:zip_code]
+    @forecast = get_weather(zip_code)
+  end
+
+  private
+
+  def get_weather(zip_code)
+    # get weather
+  end
 end
